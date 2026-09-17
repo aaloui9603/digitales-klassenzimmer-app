19dd8eb3413f79660119e7ac131856dffcb2cb06
aaloui9603 <aaloui.9603@gmail.com>
2026-09-17 14:08:30 +0200

    Feature: M3 - Stores definieren

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ClassroomView from '@/views/ClassroomView.vue'
+import StudyPlanerView from '@/views/StudyplanerView.vue'
+import ExamSimulatorView from '@/views/ExamSimulatorView.vue'
+import VocableTrainerView from '@/views/VocableTrainerView.vue'
+import LearningTrackerView from '@/views/LearningTrackerView.vue'
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'home', component: HomeView},
+        { path: '/classroom', name: 'classroom', component: ClassroomView},
+        { path: '/studyplaner', name: 'studyplaner', component: StudyPlanerView},
+        { path: '/examsimulator', name: 'examsimulator', component: ExamSimulatorView},
+        { path: '/vocabletrainer', name: 'vocabletrainer', component: VocableTrainerView},
+        { path: '/learningtracker', name: 'learningtracker', component: LearningTrackerView}
+    ],
+    scrollBehavior() {
+        return { top: 0 }
+    },
+})
+
+export default router
