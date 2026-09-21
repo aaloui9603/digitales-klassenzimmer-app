b3e03aad1a3423787c7163e7bc6c5eedcdb01d5c
aaloui9603 <aaloui.9603@gmail.com>
2026-09-21 11:31:27 +0200

    Feat 6 - Einführung Google Font und Anpassung HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,10 @@ const isDark = useDarkMode()
 <template>
     <h1 class="text-red-600">HomeView</h1>
     <button @click="isDark =!isDark">Darkmode umschalten</button>
+
+    <div class="w-96 mx-auto mt-30 h-64 border-2 border-dashed">Hier erscheint später ein Bild.
+    </div>
+
 </template>
 
 <style scoped>
