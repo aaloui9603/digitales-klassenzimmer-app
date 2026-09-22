dfcd04e93fea39a65e0ab0e61ce2408dd2bc2db6
aaloui9603 <aaloui.9603@gmail.com>
2026-09-22 12:10:03 +0200

    Feat 9: Neumorphism-Styling, classroomstore.js Anpassung

--- src/stores/classroomStore.js
+++ src/stores/classroomStore.js
@@ -1,3 +1,4 @@
+
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '../composables/useLocalStorage.js'
 
@@ -5,12 +6,17 @@ export const useClassroomStore = defineStore('classroom', () => {
  const students = useLocalStorage('students', [])
 
  function addStudent(name) {
-  students.value.push(name)
+  students.value.push({ name: name, grades: [] })
+}
+
+function addGrade(studentIndex, grade) {
+students.value[studentIndex].grades.push(grade)
 }
 
  function removeStudent(index) {
    students.value.splice(index, 1)
 }
 
-return { students, addStudent, removeStudent } 
+return { students, addStudent, addGrade, removeStudent } 
 })
+
--- src/views/ClassroomView.vue
+++ src/views/ClassroomView.vue
@@ -4,12 +4,19 @@ import { useClassroomStore } from '@/stores/classroomStore.js'
 
 const classroomStore = useClassroomStore()
 const newStudentName = ref('')
+const newGrades = ref([])
 
 function handleAdd() {
   if (newStudentName.value.trim() === '') return
   classroomStore.addStudent(newStudentName.value)
   newStudentName.value = ''
 }
+
+function handleAddGrade(index) {
+classroomStore.addGrade(index, Number(newGrades.value[index]))
+newGrades.value[index] = ''
+}
+
 </script>
 
 <template>
@@ -21,9 +28,17 @@ function handleAdd() {
  <button @click="handleAdd">Eintragen</button>
 
 <ul>
+  
    <li v-for="(student, index) in classroomStore.students" :key="index">
-     {{ student }}
+     {{ student.name }}
    <button @click="classroomStore.removeStudent(index)">Löschen</button>
+
+
+<div class="bg-dodgerblue/10 backdrop-blur-md border border-gray76 shadow-[4px_4px_10px_rgba(0,0,0,0.15),-4px_-4px_10px_rgba(255,255,255,0.7)] rounded-lg p-4">
+   Noten: {{ student.grades.join(',') }}
+   <input v-model="newGrades[index]" type="number" />
+   <button @click="handleAddGrade(index)">Note eintragen</button>
+</div>
   </li>
 </ul>
 </div>
@@ -31,4 +46,3 @@ function handleAdd() {
 
 <style scoped>
 </style>
-
