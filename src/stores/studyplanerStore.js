0bda62213887a9df684ba010709f4a992bd7f01d
aaloui9603 <aaloui.9603@gmail.com>
2026-09-22 19:17:40 +0200

    Feat 11: StudyplanerView.vue und studyplanerStore.js angepasst und Glassmorphism-Fläche hinzugefügt.

--- src/stores/studyplanerStore.js
+++ src/stores/studyplanerStore.js
@@ -1,4 +1,19 @@
+
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '../composables/useLocalStorage.js'
 
 export const useStudyplanerStore = defineStore('studyplaner', () => {
+  const sessions = useLocalStorage('sessions', [])
+
+ function addSession(subject, date) {
+  sessions.value.push({ subject: subject, date: date })
+}
+
+ function removeSession(index) {
+   sessions.value.splice(index, 1)
+}
+
+
+return { sessions, addSession, removeSession } 
 })
+
--- src/views/StudyplanerView.vue
+++ src/views/StudyplanerView.vue
@@ -1,9 +1,32 @@
 <script setup>
+import { ref } from 'vue'
+import { useStudyplanerStore } from '@/stores/studyplanerStore.js'
+
+const studyplanerStore = useStudyplanerStore()
+const newSubject = ref('')
+const newDate = ref('')
+
+function handleAdd() {
+  if (newSubject.value.trim() === '') return
+  studyplanerStore.addSession(newSubject.value, newDate.value)
+  newSubject.value = ''
+  newDate.value = ''
+}
 </script>
 
 <template>
-    <h1 class="text-purple-600">StudyPlaner View</h1>
-</template>
+<div class="bg-royalblue2/15 backdrop-blur-lg border border-royalblue2/30 shadow-xl rounded-3xl p-6">
+  <input v-model="newSubject" placeholder="Fach" class="mr-4" />
+  <input v-model="newDate" type="date" class="mr-4" />
+  <button @click="handleAdd">Eintragen</button>
 
-<style scoped>
-</style>
+ <ul>
+   <li v-for="(session, index) in studyplanerStore.sessions" :key="index">
+        {{ session.subject }} {{ session.date }}
+        <button @click="studyplanerStore.removeSession(index)">Löschen</button>
+   </li>
+ </ul>
+ </div>
+
+
+</template>
