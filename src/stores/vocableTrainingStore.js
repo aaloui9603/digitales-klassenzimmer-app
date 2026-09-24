ac2558be23d988e051e34fc1906a46ae9263c430
aaloui9603 <aaloui.9603@gmail.com>
2026-09-24 17:15:10 +0200

    Feat 17: Spaced-Repetition VocableTrainer

--- src/stores/vocableTrainingStore.js
+++ src/stores/vocableTrainingStore.js
@@ -1,4 +1,23 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+
 
 export const useVocableTrainingStore = defineStore('vocableTraining', () => {
+  const selectedLanguagePair = ref('de-en')
+  const vocabulary = [
+    { word: 'Haus', translation: 'house', interval: 1 },
+    { word: 'Buch', translation: 'book', interval: 1}
+]
+
+function updateInterval(vocabIndex, wasCorrect) {
+  if (wasCorrect) {
+     vocabulary[vocabIndex].interval = vocabulary[vocabIndex].interval * 2
+  } else {
+   vocabulary[vocabIndex].interval = 1
+}
+}
+
+return { vocabulary, updateInterval, selectedLanguagePair }
+
 })
--- src/views/VocableTrainingView.vue
+++ src/views/VocableTrainingView.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { useVocableTrainingStore } from '@/stores/vocableTrainingStore.js'
+
+const vocableTrainingStore = useVocableTrainingStore()
 </script>
 
 <template>
     <h1 class="text-yellow-600">Vocable Training View</h1>
+
+<ul>
+  <li v-for="(vocab, index) in vocableTrainingStore.vocabulary" :key="index">
+    {{ vocab.word }} - {{ vocab.translation }} (Intervall: {{ vocab.interval }})
+    <button @click="vocableTrainingStore.updateInterval(index, true)" class="mr-4">Richtig</button>
+    <button @click="vocableTrainingStore.updateInterval(index, false)">Falsch</button>
+  </li>
+</ul>
 </template>
 
 <style scoped>
 </style>
+
