56561c25d6cc6c9b7b705b8a0c983895b57a01ff
aaloui9603 <aaloui.9603@gmail.com>
2026-09-25 15:07:11 +0200

    TF 2 - Bildslider HomeView + klickbare Punkte

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,38 @@
 <script setup>
+import { ref } from 'vue'
 import { useDarkMode } from '@/composables/useDarkMode.js' 
+import slide1 from '@/assets/Retro_Symbol_School.jpeg'
+import slide2 from '@/assets/classroom_people.jpeg'
+import slide3 from '@/assets/digital-classroom.jpeg'
+
+const slides = [slide1, slide2, slide3]
 
 const isDark = useDarkMode()
+
+const currentSlide = ref(0)
+
+function nextSlide() {
+   currentSlide.value = (currentSlide.value + 1) % slides.length
+}
+
+setInterval(nextSlide, 4000)
 </script>
 
 <template>
     <h1 class="text-red-600">HomeView</h1>
     <button @click="isDark =!isDark">{{ $t('darkModeToggle') }}</button>
 
-    <div class="w-96 mx-auto mt-30 h-64 border-2 border-dashed">Hier erscheint später ein Bild.
-    </div>
+    <img :src="slides[currentSlide]" class="w-96 mx-auto mt-15 h-64 object-cover rounded-lg" alt="Klassenraum-Impression" />
+
+  <div class="flex justify-center gap-2 mt-4">
+  <button
+    v-for="(slide, index) in slides"
+    :key="index"
+    @click="currentSlide = index"
+    :class="index === currentSlide ? 'bg-dodgerblue' : 'bg-gray70'"
+    class="w-3 h-3 rounded-full"
+  ></button>
+</div>  
 
 </template>
 
