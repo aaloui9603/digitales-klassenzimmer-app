26ef4e3025eb9172bfc953ce178adba260c9ae24
aaloui9603 <aaloui.9603@gmail.com>
2026-09-25 16:33:21 +0200

    TF 4 - Burgermenü-Untermenü: Glassmorphism

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -13,8 +13,8 @@ function toggleMenu() {
 
 <template>
     <nav class="flex justify-end flex-col items-end">
-    <button :aria-expanded="isMenuOpen" @click="toggleMenu" aria-label="Menü öffnen">☰</button>
-    <ul v-if="isMenuOpen">
+    <button :aria-expanded="isMenuOpen" @click="toggleMenu" aria-label="Menü öffnen" class="bg-marineblau/20 backdrop-blur-md border border-marineblau/30 rounded-full w-10 h-10 text-white">☰</button>
+    <ul v-if="isMenuOpen" class="bg-marineblau/20 backdrop-blur-lg border border-marineblau/30 shadow-xl rounded-2xl p-4 mt-2 text-white">
         <li><RouterLink to="/classroom">Klassenzimmer-Verwaltungstool</RouterLink></li> 
         <li><RouterLink to="/studyplaner">Lernplan</RouterLink></li>
         <li><RouterLink to="/examsimulator">Prüfungssimulator</RouterLink></li>
